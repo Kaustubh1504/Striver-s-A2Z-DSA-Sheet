8703335ca2d3819bb7241edde48872966fb1907f
Kaustubh Gharat <122547394+Kaustubh1504@users.noreply.github.com>
2023-03-05 11:31:36 +0530

    Add files via upload

---
+++ array/majority_element.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+
+        // map<int,int> freq;
+        // for(int i=0;i<nums.size();i++){
+        //     freq[nums[i]]++;
+        // }
+
+        // int majcont=nums.size()/2;
+        // int ans=0;
+
+        // for(auto itr: freq){
+        //     if(itr.second>majcont){
+        //         ans=itr.first;
+        //         break;
+        //     }
+        // }
+
+        // return ans;
+
+        // int ans=nums[0];
+        // int n = nums.size();
+        // for(int i=1;i<n;i++){
+            
+
+        //     if(nums[i]!=nums[i-1]){
+        //         ans=nums[i];
+        //     }else{
+        //         ans=ans;
+        //     }
+        // }
+
+
+        return ans;
+        
+    }
+};
