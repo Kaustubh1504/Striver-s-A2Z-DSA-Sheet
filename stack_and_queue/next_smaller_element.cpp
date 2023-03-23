1d20199b3745155dd1e009e1571861754b934781
Kaustubh Gharat <122547394+Kaustubh1504@users.noreply.github.com>
2023-03-23 14:50:39 +0530

    Create next_smaller_element.cpp

---
+++ stack_and_queue/next_smaller_element.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	void immediateSmaller(vector<int>&arr, int n) {
+	    //  code here
+	   // stack<int> st;
+	   // vector<int> ans(n,-1);
+	    
+	   // for(int i=0;i<n;i++){
+	   //     while(!st.empty() && arr[st.top()]>arr[i]){
+	   //         ans[st.top()]=arr[i];
+	   //         st.pop();
+	   //     }
+	   //     st.push(i);
+	   // }
+	    
+	   // for(int i=0;i<n;i++) {
+	   //     arr[i]=ans[i];
+	   // }
+	   
+	   //for(int i=0;i<n-1;i++){
+	   //    if(arr[i+1]<arr[i]){
+	   //        arr[i]=arr[i+1];
+	   //    }else{
+	   //        arr[i]=-1;
+	   //    }
+	   //}
+	   //arr[n-1]=-1;
+	   
+	   
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, l, r;
+        cin >> n;
+        vector<int>arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.immediateSmaller(arr, n);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
