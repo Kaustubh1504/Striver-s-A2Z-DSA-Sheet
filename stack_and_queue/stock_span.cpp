63edea5d3181b092253ebefc105084d6ffd7b5db
Kaustubh Gharat <122547394+Kaustubh1504@users.noreply.github.com>
2023-03-24 11:25:42 +0530

    Create stock_span.cpp

---
+++ stack_and_queue/stock_span.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to calculate the span of stockâ€™s price for all n days.
+    vector <int> calculateSpan(int price[], int n)
+    {
+       // Your code here
+       vector<int> PGE(n,-1);
+       stack<int> st;
+    
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && price[st.top()]<price[i]){
+                PGE[st.top()]=i;
+                st.pop();
+            }
+            st.push(i);
+            
+        }
+        
+        vector<int> ans(n,-1);
+        for(int i=n-1;i>=0;i--){
+            ans[i]=i-PGE[i];
+        }
+        
+        return ans;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int i,a[n];
+		for(i=0;i<n;i++)
+		{
+			cin>>a[i];
+		}
+		Solution obj;
+		vector <int> s = obj.calculateSpan(a, n);
+		
+		for(i=0;i<n;i++)
+		{
+			cout<<s[i]<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
