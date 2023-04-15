d472940a3f75de57855799287323d6d4219771d6
Kaustubh Gharat <122547394+Kaustubh1504@users.noreply.github.com>
2023-04-15 21:19:43 +0530

    Create median_of_2_sorted_array.cpp

---
+++ binary_search/median_of_2_sorted_array.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+
+        // vector<int> mer;
+
+        // int m=nums1.size();
+        // int n=nums2.size();
+
+        // int l=0;
+        // int r=0;
+
+        // while(l<m && r<n){
+        //     if(nums1[l]<nums2[r]){
+        //         mer.push_back(nums1[l]);
+        //         l++;
+        //     }else if(nums1[l]>nums2[r]){
+        //         mer.push_back(nums2[r]);
+        //         r++;
+        //     }else{
+        //          mer.push_back(nums1[l]);
+        //          mer.push_back(nums2[r]);
+        //          l++;
+        //          r++;
+        //     }
+        // }
+        // while(l<m){
+        //     mer.push_back(nums1[l]);
+        //     l++;
+        // }
+
+        // while(r<n){
+        //     mer.push_back(nums2[r]);
+        //     r++;
+        // }
+
+        // int len=mer.size();
+        // // for(auto c: mer) cout<<c<<" ";
+        // // cout<<m+n;
+        // double median=(mer[len/2]+mer[(len-1)/2])/2.0;
+
+        // return median;
+
+        int m=nums1.size();
+        int n=nums2.size();
+
+
+
+
+       
+
+        
+    }
+};
